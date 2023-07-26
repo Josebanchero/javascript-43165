6099e6bdce6aba526a0a6530e481206028e9e743
Josebanchero <jose.banchero2@gmail.com>
2023-07-26 20:58:43 -0300

    Segunda Entrega Proyecto Final

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Info personal */
 let nombreIngresado = prompt ("Ingresa tu nombre")
 alert ("El nombre ingresado es " + nombreIngresado)
 console.log (nombreIngresado)
@@ -10,9 +11,13 @@ let edadIngreasada = prompt ("Ingresa tu edad")
 alert ("La edad ingresada es " + edadIngreasada)
 console.log (edadIngreasada)
 
+/* e-mail */
+
 let emailIngresado = prompt ("Ingresa tu e-mail")
 alert ("El e-mail ingresado es " + emailIngresado)
 console.log (emailIngresado)
+
+/* Contraseña */
 
 
 while (true){
@@ -29,12 +34,40 @@ while (true){
     }
 }
 
-
-
-
+/* Usuario creado */
 
 alert ("Usuario: " + nombreIngresado + apellidoIngresado + edadIngreasada)
 alert ("E-mail: " + emailIngresado)
 alert ("Contraseña: *******")
 
 
+
+/* CARRITO */
+/* Productos */
+ const productos = [
+    {nombre: "Bandeja Amelia", precio: 5000 },
+    {nombre: "Maceta Robert", precio: 4500},
+    {nombre: "Bandeja Multiple", precio: 7500},
+    
+
+ ]
+ console.log(productos)
+
+ const resultado = productos.find((el) => el.nombre === "Bandeja Amelia")
+ const resultado2 = productos.find((el) => el.nombre === "Bandeja Uxi")
+
+console.log(resultado)
+console.log(resultado2)
+
+/* Ingresando productos */
+let productoIngresado = prompt ("Ingresa el producto que quieras")
+let cantidadIngresada = prompt ("Ingresa la cantidad del producto elegido")
+alert ("Se agrego a tu carrito " + cantidadIngresada + " " + productoIngresado)
+console.log (cantidadIngresada + productoIngresado)
+
+/* Total */
+
+const precio = [5000, 4500, 7500]
+const total = precio.reduce((acumulador, elemento)=> acumulador + elemento, 0)
+alert("Tu total es: " + total)
+console.log(total)
